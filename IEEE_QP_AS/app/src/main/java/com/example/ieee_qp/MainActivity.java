129e3da9828f695151bdf1855c62cbb3c287c70c
Martin Zeng <540288198@qq.com>
2020-01-25 12:45:11 -0800

    Added an empty android studio project

---
+++ IEEE_QP_AS/app/src/main/java/com/example/ieee_qp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ieee_qp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
