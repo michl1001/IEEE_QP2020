7ebf1ae5fd28e70bc73cc169b0848843fbe14db7
Martin Zeng <540288198@qq.com>
2020-01-30 14:29:18 -0800

    Login UI completed

---
+++ IEEE_QP_AS/app/src/main/java/com/example/ieee_qp/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.example.ieee_qp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button button;
+    private EditText usernameEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        button = (Button) findViewById(R.id.loginBtn);
+        usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(login);
+                finish();
+            }
+        });
+    }
+}
