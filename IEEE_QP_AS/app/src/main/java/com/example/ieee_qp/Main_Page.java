6ebf9c9c0c497b77f360297db6420238aef844a4
carolbao <y1bao@ucsd.edu>
2020-02-23 13:33:43 -0800

    New Add activity page

---
+++ IEEE_QP_AS/app/src/main/java/com/example/ieee_qp/Main_Page.java
@@ -0,0 +1,14 @@
+package com.example.ieee_qp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__page);
+    }
+}
