31d5dcd67153ad3efc35188c46a596b483a2de03
Martin Zeng <540288198@qq.com>
2020-02-23 13:55:39 -0800

    Renamed main page

--- IEEE_QP_AS/app/src/main/java/com/example/ieee_qp/MainActivity.java
+++ IEEE_QP_AS/app/src/main/java/com/example/ieee_qp/MainActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Main_Page extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main__page);
+        setContentView(R.layout.activity_main);
     }
 }
